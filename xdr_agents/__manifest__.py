aa4d3f9ea195386777a4947397810959842f2470
isaacglezr6 <beto.gr7@gmail.com>
2023-11-07 21:50:52 +0000

    [ADD] Ticket131 Se añade modulo para agentes xdr

---
+++ xdr_agents/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "XDR Agents",
+    'summary': "App for XDR Agents implementation",
+    'description': "XDR Agents",
+    'author':"HackDoo",
+    'website': "https://hackdoo.com/",
+    'category': 'Uncategorized',
+    'version': '16.0',
+    'depends': [
+        'base',
+        'backend_iframe',
+    ],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/agents_views.xml',
+        'views/menuitems.xml',
+    ],
+    # 'assets':{
+    #     'web.assets_backend':[
+        
+    #     ]
+    # }
+}
