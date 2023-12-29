c52280a059fe47018c6344e92afa74350a574515
isaacglezr6 <beto.gr7@gmail.com>
2023-12-29 17:48:19 +0000

    [FIX] Se quita $ en contraseña y se ponen readonly campos

--- grcbit_seller/models/res_partner.py
+++ grcbit_seller/models/res_partner.py
@@ -247,7 +247,7 @@ class ResPartnerGRC(models.Model):
             # )
             random_pass = (
                 [
-                    random.choice("@$!%*?&-_"),
+                    random.choice("@!%*?&-_"),
                     random.choice(string.digits),
                     random.choice(string.ascii_lowercase),
                     random.choice(string.ascii_uppercase),
@@ -256,7 +256,7 @@ class ResPartnerGRC(models.Model):
                     random.choice(
                         string.ascii_lowercase
                         + string.ascii_uppercase
-                        + "@$!%*?&-_"
+                        + "@!%*?&-_"
                         + string.digits
                     ) for i in range(12)
                 ]
