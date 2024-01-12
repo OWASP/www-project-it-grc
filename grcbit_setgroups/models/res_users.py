5bde22183ee6ae8db96571023ab4024fa42ac34d
isaacglezr6 <beto.gr7@gmail.com>
2024-01-12 21:47:06 +0000

    [FIX] Se agrega bolean para support y su restriccion

--- grcbit_setgroups/models/res_users.py
+++ grcbit_setgroups/models/res_users.py
@@ -5,10 +5,12 @@ from odoo.exceptions import ValidationError
 class ResUsersInh(models.Model):
     _inherit = 'res.users'
 
+    is_support = fields.Boolean(string="Is Support", default=False)
+
     def write(self, vals):
         res = super(ResUsersInh, self).write(vals)
         user_id = self.env.user
-        if user_id.name != 'support' or user_id.login != 'support@grcbit.com':
-            if self.name == 'support'  or self.login == 'support@grcbit.com':
+        if user_id.is_support != True:
+            if self.is_support == True:
                 raise ValidationError("This users can't be update for you")
         return res
