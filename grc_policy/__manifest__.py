fb5397742f8b5f8a13c21c335353ea770558c8db
isaacglezr6 <beto.gr7@gmail.com>
2024-04-18 18:29:19 +0000

    [ADD] Se genera modulo grc policy (falta reporte)

---
+++ grc_policy/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "GRCBIT POLICY",
+    'summary': "App for grcbit policy implementation",
+    'description': "POLICYS",
+    'author':"HackDoo",
+    'website': "https://hackdoo.com/",
+    'category': 'Uncategorized',
+    'version': '16.0',
+    'depends': [
+        'grcbit_risk_management',
+        'document_page_approval',
+        'document_page',
+    ],
+
+    # always loaded
+    'data': [
+        #reports
+        'reports/document_page_report.xml',
+        
+        #views
+        'views/document_page_views.xml',
+        'views/menuitems.xml',
+
+    ],
+ 
+}
