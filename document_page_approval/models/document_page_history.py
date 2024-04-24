88d2a26d1878ded0a6fec380503ef6dd9a7f03a3
isaacglezr6 <beto.gr7@gmail.com>
2024-04-24 20:59:36 +0000

    [FIX] Ticket 603 Se arregla traducciones de OCA

--- document_page_approval/models/document_page_history.py
+++ document_page_approval/models/document_page_history.py
@@ -1,9 +1,9 @@
 # Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import fields, models, _
 from odoo.exceptions import UserError
-from odoo.tools.translate import _
+# from odoo.tools.translate import _
 
 
 class DocumentPageHistory(models.Model):
