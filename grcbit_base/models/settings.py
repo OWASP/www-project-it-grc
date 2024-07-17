a887777063dfae206c48095bfdfc918b3f059c76
isaacglezr6 <beto.gr7@gmail.com>
2024-07-17 22:17:54 +0000

    [ADD] Se añade tcp ports #763

--- grcbit_base/models/settings.py
+++ grcbit_base/models/settings.py
@@ -33,3 +33,28 @@ class ItComponents(models.Model):
             if vals['name'] in [x.name for x in components]:
                 raise ValidationError("IT Component name already exist.!")
         return res
+
+class TCPPorts(models.Model):
+    _name = 'tcp.ports'
+
+    name = fields.Char(string="Name")
+    it_inventory_id = fields.Many2one('it.inventory', string="IT Components")
+    color = fields.Integer(string="Color", default=lambda x: x.default_color())
+
+    _sql_constraints = [
+        ('unique_name','unique(name)','TCP Port name already exist.!')]
+
+    def default_color(self):
+        x = r.randrange(11)
+        if x <= 11 or x > 0:
+            return x
+        
+    @api.model
+    def create(self, vals):
+        res = super(TCPPorts, self).create(vals)
+        tcp_port = self.env['tcp.ports'].search([('id','!=', res.id)])
+        if tcp_port:
+            if vals['name'] in [x.name for x in tcp_port]:
+                raise ValidationError("TCP Port name already exist.!")
+        return res
+    
